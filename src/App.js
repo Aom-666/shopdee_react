bfd9dd4831ed00a058a256161ab83c0efefa0f06
Vachiravit <Vachiravitcp1@gmail.com>
2024-09-18 01:20:14 +0700

    v1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
+
+
 import './App.css';
+import Dashboard from './components/dashbord.js';
+import EmployeeLogin from './components/Employeelogin.js';
 import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import AddEmp from './components/Addemp.js';
 
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route exact path='/' element={<EmployeeLogin/>}/>
+        <Route exact path='/Addemp' element={<AddEmp/>}/>
+        <Route exact path='/dashboard' element={<Dashboard/>}/>
+        <Route exact path='/EmployeeLogin' element={<EmployeeLogin/>}/>
         <Route exact path='/signin' element={<SignIn/>}/> 
         <Route exact path='/signup' element={<SignUp/>}/>
 
